553ecffc955d931cba8c67203bf79b8705aafed7
Clinton <davelaarprogramming@gmail.com>
2019-10-01 18:14:39 -0500

    test

---
+++ Cheap Shoppers/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Cheap Shoppers
+//
+//  Created by Davelaar,Clinton B on 10/1/19.
+//  Copyright © 2019 Davelaar,Clinton B. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Cheap Shoppers/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Cheap Shoppers
+//
+//  Created by Davelaar,Clinton B on 10/1/19.
+//  Copyright © 2019 Davelaar,Clinton B. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
