f77d796e32cd93036b276820f4355b45db577131
rajesh007 <s534844@nwmissouri.edu>
2019-10-01 19:31:28 -0500

    Created a Item Lookup screen

---
+++ Cheap Shoppers/ItemLookUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ItemLookUpViewController.swift
+//  Cheap Shoppers
+//
+//  Created by Student on 10/1/19.
+//  Copyright © 2019 Davelaar,Clinton B. All rights reserved.
+//
+
+import UIKit
+
+class ItemLookUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
