60f2a8979bcd7b3fcb31e7a9430a5ad03cd65e50
echtniet <davelaarprogramming@gmail.com>
2019-10-19 16:39:37 -0500

    Friend singleton

--- Cheap Shoppers/FriendBook.swift
+++ Cheap Shoppers/FriendBook.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+
+struct Friend{
+    var firstName:String
+    var lastName:String
+    var phone:String
+}
+
+class FriendBook {
+    private var _friends:[Friend]
+    
+    var friends:[Friend]{
+        return _friends
+    }
+    
+    var numFriends:Int{
+        return _friends.count
+    }
+    
+    private static var _shared:FriendBook!
+    
+    static var shared:FriendBook{
+        if _shared == nil{
+            _shared = FriendBook()
+        }
+        return _shared
+    }
+    
+    subscript(i:Int)-> Friend{
+        return _friends[i]
+    }
+    
+    func add(friend:Friend)throws{
+        
+        _friends.append(friend)
+        _friends.sort { (friendA, friendB) in return friendA.lastName < friendB.lastName }
+    }
+    
+    private init(){
+        _friends = [Friend(firstName: "Clinton", lastName: "Davelaar", phone: "0123456789"),
+                    Friend(firstName: "Mike", lastName: "Burnes", phone: "9876543210")]
+    }
+    
+    
+}
+
+enum FriendError:Error{
+    case NegativeDistance, NegativeNumMoons
+}
