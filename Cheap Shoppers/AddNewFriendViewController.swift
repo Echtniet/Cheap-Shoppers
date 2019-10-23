a40599de013ebad14288e41320ecd1ee602576dc
Clinton Davelaar <s533350@nwmissouri.edu>
2019-10-22 23:03:02 -0500

    added AddNewFriendViewController

---
+++ Cheap Shoppers/AddNewFriendViewController.swift
@@ -0,0 +1,61 @@
+//
+//  AddNewFriendViewController.swift
+//  Cheap Shoppers
+//
+//  Created by Student on 10/22/19.
+//  Copyright © 2019 Davelaar,Clinton B. All rights reserved.
+//
+
+import UIKit
+
+class AddNewFriendViewController: UIViewController {
+    
+    @IBOutlet weak var firstNameTF: UITextField!
+    @IBOutlet weak var lastNameTF: UITextField!
+    @IBOutlet weak var phoneNumTF: UITextField!
+    
+    @IBAction func addNew(_ sender: UIButton) {
+        let firstName:String = String(self.firstNameTF.text!)
+        let lastName:String = String(self.lastNameTF.text!)
+        let phoneNum:String = String(self.phoneNumTF.text!)
+        
+        FriendBook.shared.add(friend: Friend(firstName: firstName, lastName: lastName, phone: phoneNum))
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Add New Friend"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(add))
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func cancel() {
+        
+    }
+    
+    @objc func add() {
+        let firstName = firstNameTF.text
+        let lastName = lastNameTF.text
+        let phoneNum = phoneNumTF.text
+        
+        let friend = Friend(firstName: firstName!, lastName: lastName!, phone: phoneNum!)
+        FriendBook.shared.add(friend: Friend)
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
